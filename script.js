e88f49cf8d184147959f234b39462e2f7ad14fd9
Shoumodip Kar <shoumodipkar@gmail.com>
2023-08-17 21:05:59 +0530

    Introduce master

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const game = {}
 
-function drawMan(x, y, s) {
+function drawPicker(x, y, s) {
   game.ctx.beginPath()
   game.ctx.moveTo(x + s * 0.13, y)
   game.ctx.lineTo(x + s * 0.28, y)
@@ -28,8 +28,42 @@ function drawMan(x, y, s) {
   game.ctx.fill()
 }
 
+function drawMaster(x, y, s) {
+  game.ctx.beginPath()
+  game.ctx.moveTo(x + s * 0.13, y)
+  game.ctx.lineTo(x + s * 0.28, y)
+  game.ctx.lineTo(x + s * 0.28, y - s * 0.5)
+  game.ctx.lineTo(x + s * 0.30, y - s * 0.5)
+  game.ctx.lineTo(x + s * 0.30, y - s * 0.9)
+
+  game.ctx.lineTo(x + s * 0.28, y - s * 0.9)
+  game.ctx.lineTo(x + s * 0.28, y - s * 1.05)
+  game.ctx.lineTo(x + s * 0.13, y - s * 1.05)
+  game.ctx.lineTo(x + s * 0.13, y - s * 0.9)
+  game.ctx.lineTo(x + s * 0.12, y - s * 0.9)
+  game.ctx.lineTo(x + s * 0.12, y - s * 0.5)
+  game.ctx.lineTo(x + s * 0.12, y - s * 0.5)
+  game.ctx.lineTo(x + s * 0.13, y - s * 0.5)
+  game.ctx.closePath()
+
+  game.ctx.fillStyle = "#FFD6A4"
+  game.ctx.fill()
+
+  game.ctx.beginPath()
+  game.ctx.moveTo(x + s * 0.12, y - s * 0.7)
+  if (game.beating) {
+    game.ctx.lineTo(x - s * 0.42, y - s * 0.65)
+  } else {
+    game.ctx.lineTo(x - s * 0.42, y - s * 0.8)
+  }
+
+  game.ctx.closePath()
+  game.ctx.strokeStyle = "#000000"
+  game.ctx.stroke()
+}
+
 function drawGame() {
-  const size = Math.min(game.app.width, game.app.height) * 0.5
+  game.size = Math.min(game.app.width, game.app.height) * 0.5
 
   game.ctx.fillStyle = "#87CEEB"
   game.ctx.fillRect(0, 0, game.app.width, game.app.height)
@@ -37,26 +71,52 @@ function drawGame() {
   game.ctx.fillStyle = "#388004"
   game.ctx.fillRect(0, game.app.height * 0.8, game.app.width, game.app.height * 0.2)
 
-  game.ctx.font = size * 0.12 + "px Serif"
+  game.ctx.font = game.size * 0.12 + "px Serif"
   game.ctx.fillStyle = "#000000"
-  game.ctx.fillText(game.cotton, size * 0.2, size * 0.15)
+  game.ctx.fillText(game.cotton, game.size * 0.2, game.size * 0.15)
 
   game.ctx.beginPath()
   if (game.picking) {
-    game.ctx.arc(size * 0.11, size * 0.11, size * 0.06, 0, 2 * Math.PI)
+    game.ctx.arc(game.size * 0.11, game.size * 0.11, game.size * 0.06, 0, 2 * Math.PI)
   } else {
-    game.ctx.arc(size * 0.11, size * 0.11, size * 0.05, 0, 2 * Math.PI)
+    game.ctx.arc(game.size * 0.11, game.size * 0.11, game.size * 0.05, 0, 2 * Math.PI)
   }
   game.ctx.closePath()
 
   game.ctx.fillStyle = "#FDF3EA"
   game.ctx.fill()
 
-  game.ctx.lineWidth = size * 0.01
+  game.ctx.lineWidth = game.size * 0.01
   game.ctx.strokeStyle = "#000000"
   game.ctx.stroke()
 
-  drawMan(game.app.width * 0.5, game.app.height * 0.8, size)
+  drawPicker(game.app.width * 0.5, game.app.height * 0.8, game.size)
+
+  if (game.master) {
+    drawMaster((game.app.width + game.size) * 0.5, game.app.height * 0.8, game.size * 1.2)
+  }
+}
+
+function pickCotton() {
+  if (!game.picking) {
+    game.cotton += 1
+    game.picking = true
+
+    drawGame()
+    setTimeout(() => {
+      game.picking = false
+      drawGame()
+    }, 50)
+
+    if (game.cotton == 50) {
+      game.master = true
+      setInterval(() => {
+        game.beating = true
+        pickCotton()
+        game.beating = false
+      }, 1000)
+    }
+  }
 }
 
 window.onload = () => {
@@ -64,18 +124,16 @@ window.onload = () => {
   game.ctx = game.app.getContext("2d")
 
   game.cotton = 0
+  game.master = false
+  game.beating = false
   game.picking = false
 
-  window.onclick = () => {
-    if (!game.picking) {
-      game.cotton += 1
-      game.picking = true
+  window.onclick = (e) => {
+    const x = e.offsetX - game.app.width * 0.5 + game.size * 0.5
+    const y = e.offsetY - game.app.height * 0.8 + game.size
 
-      drawGame()
-      setTimeout(() => {
-        game.picking = false
-        drawGame()
-      }, 50)
+    if (x > 0 && x < game.size && y > 0 && y < game.size) {
+      pickCotton()
     }
   }
 
